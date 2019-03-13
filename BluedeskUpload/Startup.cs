5f16f462bfda9ee288e3c94b607e5cca61c2fa1d
Jordi <jordiva.8284@gmail.com>
2019-03-13 11:24:20 +0100

    Add project files.

---
+++ BluedeskUpload/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BluedeskUpload.Startup))]
+namespace BluedeskUpload
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
