952c9813c8ac6fba9bc85a278479c3a0cf5f9fac
Yara Weening <yaraweening1@gmail.com>
2019-05-09 12:53:11 +0200

    Rol uploader en downloader toegevoegd en favicon aangepast.

--- BluedeskUpload/Startup.cs
+++ BluedeskUpload/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using BluedeskUpload.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(BluedeskUpload.Startup))]
@@ -9,6 +12,25 @@ namespace BluedeskUpload
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRoles();
+        }
+
+        private void createRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Uploader"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Uploader";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Downloader"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Downloader";
+                roleManager.Create(role);
+            }
         }
     }
 }
--- BluedeskUpload/Uploads/dbo.Helloworld.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] VARCHAR(50) NOT NULL PRIMARY KEY, 
-    [name] VARCHAR(50) NOT NULL, 
-    [age] INT NOT NULL, 
-    [phone number] INT NOT NULL
-)
